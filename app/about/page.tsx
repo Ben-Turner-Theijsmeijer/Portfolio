0b773a3b686324667b7a3c69855c819531e040f3
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-05-04 21:30:10 -0400

    Messed around with different variables

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,11 @@
 export default function experience() {
     return (
-        <h1>About</h1>
+        <div className="relative min-h-[90vh] overflow-hidden">
+            <h1>About</h1>
+            <div className="absolute bottom-0 left-75 p-30 w-screen -z-10 rotate-285 bg-green-200">
+            </div>
+            <div className="absolute bottom-0 left-25 p-5 w-screen -z-10 rotate-285 bg-green-200">
+            </div>
+        </div>
     );
 }
