ed8f753d20d385db3788362cb2380003e59f1edb
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-05-05 16:50:38 -0400

    Ideation on layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,21 @@
 export default function experience() {
     return (
-        <div className="relative min-h-[90vh] overflow-hidden">
-            <h1>About</h1>
-            <div className="absolute bottom-0 left-75 p-30 w-screen -z-10 rotate-285 bg-green-200">
+        <div>    
+            <div className="relative min-h-[90vh] overflow-hidden">
+                <h1>About</h1>
+                <div className="absolute bottom-0 left-75 p-30 w-screen -z-10 rotate-285 stripe">
+                </div>
+                <div className="absolute bottom-0 left-25 p-5 w-screen -z-10 rotate-285 stripe">
+                </div>
             </div>
-            <div className="absolute bottom-0 left-25 p-5 w-screen -z-10 rotate-285 bg-green-200">
+            <div className="justify-center flex">
+                <div className="p-5 bg-blue-950 w-[80vw]"></div>
             </div>
+
+            <div className="relative min-h-[90vh] overflow-hidden">
+            
         </div>
+        </div>
+        
     );
 }
