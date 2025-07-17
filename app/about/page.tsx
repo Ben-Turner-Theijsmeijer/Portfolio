188d4da205e7cbe0bd5d8898d102ae575adba8d1
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-07-17 19:10:23 -0400

    Got the motion working for the stripes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
 import { MapPin } from "lucide-react";
-// import { motion } from "motion/react"
 import * as motion from "motion/react-client"
 
 export default function AboutMe() {
+  
     return (
 
         <div className="lg:p-15 xl:p-25 flex flex-col md:flex-row items-center justify-between -z-5 p-8 bg-[#f0f5ef] overflow-hidden">
@@ -24,28 +24,58 @@ export default function AboutMe() {
             </div>
 
             <div className="md:w-2/5 flex justify-center mt-8 md:mt-0 relative">
-                    
-                {/* <motion.div 
-                initial={{ opacity: 0, x:70 }}
-                animate={{ opacity: 1, x:0 }}
-                transition={{
-                    duration: 0.8,
-                    type: "spring",
-                    stiffness: 100,
-                    damping: 10,
-                    delay: 0.2,
-                }}
-                className="Stripe">
-                </motion.div> */}
-
+                {/* Stripe section */}
                 <div className="Stripe absolute inset-0 flex justify-center items-center">
-                    <div className="absolute rotate-[260deg] md:rotate-[12deg] accent3 h-500 w-1/15 translate-y-[-3.5%] md:translate-y-[-18%]" />
-                    <div className="absolute rotate-[260deg] md:rotate-[12deg] accent1 h-500 w-1/15 translate-y-[-4%] md:translate-y-[-23%]" />
-                    <div className="absolute rotate-[260deg] md:rotate-[12deg] accent3 h-500 w-2/5 translate-y-[2%] md:translate-y-[25%]" />
-                    <div className="absolute rotate-[260deg] md:rotate-[12deg] accent2 h-500 w-2/5 translate-y-[1%] md:translate-y-[20%]" />
+                    <motion.div 
+                        className="absolute rotate-[260deg] md:rotate-[12deg] accent3 h-500 w-1/15 translate-y-[-3.5%] md:translate-y-[-18%]"
+                        variants={stripeVariants}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2,
+                        }} />
+                    <motion.div className="absolute rotate-[260deg] md:rotate-[12deg] accent1 h-500 w-1/15 translate-y-[-4%] md:translate-y-[-23%]"
+                    variants={stripeVariants}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2,
+                        }} />
+
+                    <motion.div className="absolute rotate-[260deg] md:rotate-[12deg] accent3 h-500 w-2/5 translate-y-[2%] md:translate-y-[25%]"
+                    variants={stripeVariants}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 80,
+                            damping: 10,
+                            delay: 0.4,
+                        }}  />
+                    <motion.div className="absolute rotate-[260deg] md:rotate-[12deg] accent2 h-500 w-2/5 translate-y-[1%] md:translate-y-[20%]"
+                    variants={stripeVariants}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 80,
+                            damping: 10,
+                            delay: 0.4,
+                        }}  />
                 </div>
 
 
+                {/* Portrait Section */}
                 <div className="relative w-60 h-60 sm:w-70 sm:h-70 rounded-full overflow-hidden border-r-8 border-b-8 black shadow-lg">
                 <img
                     src="images/Ben PFP.jpg"
@@ -57,3 +87,8 @@ export default function AboutMe() {
         </div>
     );  
 }
+
+const stripeVariants = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  };
