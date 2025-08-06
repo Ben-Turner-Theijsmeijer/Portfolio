d7941419d6b98e49a36624eb555a60fa1305390b
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-08-06 15:03:59 -0400

    added the static out folder

---
+++ out/_next/static/chunks/app/projects/page-9b20a02c9d4c565d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[127,220,893],{4045:(e,s,_)=>{Promise.resolve().then(_.bind(_,9638))}},e=>{var s=s=>e(e.s=s);e.O(0,[638,441,684,358],()=>s(4045)),_N_E=e.O()}]);
