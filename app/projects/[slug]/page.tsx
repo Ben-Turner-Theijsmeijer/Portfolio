e63e8ecbe92b8138eda43654288a77a2dfce03fc
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-08-30 18:38:09 -0400

    added layout fix to projects section as well

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -19,9 +19,11 @@ export default function ProjectPage({ params }: { params: Promise<{ slug: string
 
     return (
         <div>
-            <div className="w-full max-w-[1600px] mx-auto">
-                <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
-                <p className="text-gray-700">{project.content}</p>
+            <div className="w-full max-w-[1600px] mx-auto whitespace-pre-line grid grid-cols-1 justify-items-center">
+                <div className="py-25 lg:px-15 xl:px-25 w-[70%] p-8 overflow-hidden">
+                    <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
+                    <p className="text-gray-700">{project.content}</p>
+                </div>
             </div>
         </div>
     );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,19 +5,21 @@ import { ComingSoon } from "../components/comingSoon";
 export default function Projects() {
     return (
         <div>
-            <div className="w-full max-w-[1600px] mx-auto">
-                <h1 className="text-4xl font-bold mb-6">My Projects</h1>
-                <ul className="space-y-4">
-                    {projectData.map((project) => (
-                    <li key={project.slug} className="border-b pb-2">
-                        <Link href={`/projects/${project.slug}`} className="text-2xl text-blue-600 hover:underline">
-                        {project.title}
-                        </Link>
-                        <p className="text-gray-600">{project.description}</p>
-                    </li>
-                    ))}
-                </ul>
-                <ComingSoon />
+            <div className="w-full max-w-[1600px] mx-auto grid grid-cols-1 justify-items-center">
+                <div className="py-25 lg:px-15 xl:px-25 w-[70%] p-8 overflow-hidden">
+                    <h1 className="text-4xl font-bold mb-6">My Projects</h1>
+                    <ul className="space-y-4">
+                        {projectData.map((project) => (
+                        <li key={project.slug} className="border-b pb-2">
+                            <Link href={`/projects/${project.slug}`} className="text-2xl text-blue-600 hover:underline">
+                            {project.title}
+                            </Link>
+                            <p className="text-gray-600">{project.description}</p>
+                        </li>
+                        ))}
+                    </ul>
+                    <ComingSoon />
+                </div>
             </div>
         </div>
     );
