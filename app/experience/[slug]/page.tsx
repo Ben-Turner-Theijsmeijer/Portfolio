83c27371e26c2216e2f9aec62cc94e1f7e04b617
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-10-14 23:55:29 -0400

    cleaned up experience page and added goals

--- app/experience/[slug]/page.tsx
+++ app/experience/[slug]/page.tsx
@@ -20,7 +20,16 @@ export default function ExperiencePage ({ params }: { params: Promise<{ slug: st
     return (
         <div>
             <div className="w-full max-w-[1600px] mx-auto whitespace-pre-line grid grid-cols-1 justify-items-center">
-                <div className="py-25 lg:px-15 xl:px-25 w-[70%] p-8 overflow-hidden">
+                {/* breadcrumb trail */}
+                <div className="w-[70%] flex mt-10">
+                    <div className="bg-[var(--accent5)] px-2 text-[var(--nav-foreground)]">Experience</div>
+                    <div className="bg-[var(--accent3)] border-t-10 border-t-transparent border-b-10 border-b-transparent border-l-10 border-l-[var(--accent5)]"></div>
+                    <div className="bg-[var(--accent3)] px-2">{experience.slug}</div>
+                    <div className="border-t-10 border-t-transparent border-b-10 border-b-transparent border-l-10 border-l-[var(--accent3)]"></div>
+                </div>
+                {/* Article Body */}
+                <div className="my-10 w-[70%] p-4 grid grid-cols-1 justify-items-center bg-[var(--accent2)]">
+                <div className="my-5 py-25 lg:px-15 xl:px-25 w-[95%] p-8 overflow-hidden border-[var(--accent3)] border-2">
                     {/* Header */}
                     <div>
                         <h1 className="text-4xl font-bold mb-4">{experience.articleTitle}</h1>
@@ -45,7 +54,55 @@ export default function ExperiencePage ({ params }: { params: Promise<{ slug: st
                     {/* Work term Goals */}
                     <div>
                         <h2 className="text-3xl font-semibold m-4 text-center">Workterm Goals</h2>
-                        <p className="text-gray-700">{experience.goals}</p>
+                        {/* Goal 1 */}
+                        <div className="my-4">
+                            <span className="font-bold text-gray-700">{experience.goalType1}</span>
+                            <span className="text-gray-600">{experience.goal1}</span>
+                            <div className="px-15 py-5">
+                                <p className="text-gray-500 pt-2"><i><b>Action Plan:</b></i> {experience.goalActionPlan1}</p>
+                                <p className="text-gray-500 pt-4"><i><b>Reflection:</b></i> {experience.goalReflection1}</p>
+                            </div>
+                        </div>
+                        <div className="my-4 pt-0.5 w-auto bg-gray-500"></div>
+                        {/* Goal 2 */}
+                        <div className="my-4">
+                            <span className="font-bold text-gray-700">{experience.goalType2}</span>
+                            <span className="text-gray-600">{experience.goal2}</span>
+                            <div className="px-15 py-5">
+                                <p className="text-gray-500 pt-2"><i><b>Action Plan:</b></i> {experience.goalActionPlan2}</p>
+                                <p className="text-gray-500 pt-4"><i><b>Reflection:</b></i> {experience.goalReflection2}</p>
+                            </div>
+                        </div>
+                        <div className="my-4 pt-0.5 w-auto bg-gray-500"></div>
+                        {/* Goal 3 */}
+                        <div className="my-4">
+                            <span className="font-bold text-gray-700">{experience.goalType3}</span>
+                            <span className="text-gray-600">{experience.goal3}</span>
+                            <div className="px-15 py-5">
+                                <p className="text-gray-500 pt-2"><i><b>Action Plan:</b></i> {experience.goalActionPlan3}</p>
+                                <p className="text-gray-500 pt-4"><i><b>Reflection:</b></i> {experience.goalReflection3}</p>
+                            </div>
+                        </div>
+                        <div className="my-4 pt-0.5 w-auto bg-gray-500"></div>
+                        {/* Goal 4 */}
+                        <div className="my-4">
+                            <span className="font-bold text-gray-700">{experience.goalType4}</span>
+                            <span className="text-gray-600">{experience.goal4}</span>
+                            <div className="px-15 py-5">
+                                <p className="text-gray-500 pt-2"><i><b>Action Plan:</b></i> {experience.goalActionPlan4}</p>
+                                <p className="text-gray-500 pt-4"><i><b>Reflection:</b></i> {experience.goalReflection4}</p>
+                            </div>
+                        </div>
+                        <div className="my-4 pt-0.5 w-auto bg-gray-500"></div>
+                        {/* Goal 5 */}
+                        <div className="my-4">
+                            <span className="font-bold text-gray-700">{experience.goalType5}</span>
+                            <span className="text-gray-600">{experience.goal5}</span>
+                            <div className="px-15 py-5">
+                                <p className="text-gray-500 pt-2"><i><b>Action Plan:</b></i> {experience.goalActionPlan5}</p>
+                                <p className="text-gray-500 pt-4"><i><b>Reflection:</b></i> {experience.goalReflection5}</p>
+                            </div>
+                        </div>
                     </div>
                     {/* Job Description */}
                     <div>
@@ -67,6 +124,7 @@ export default function ExperiencePage ({ params }: { params: Promise<{ slug: st
                         <h2 className="text-3xl font-semibold m-4 text-center">Acknowledgements</h2>
                         <p className="text-gray-500">{experience.acknowledgements}</p>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
