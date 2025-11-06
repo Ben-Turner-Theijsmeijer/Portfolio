1e1b12229870726c85e2b46ae7946c11e89689c2
Ben Turner <97711991+Ben-Turner-Theijsmeijer@users.noreply.github.com>
2025-11-06 14:54:12 -0500

    added information to the projects section

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import projectData from "@/data/personalProjects.json";
+import * as motion from "motion/react-client"
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { use } from 'react'
@@ -42,13 +43,70 @@ export default function ProjectPage({ params }: { params: Promise<{ slug: string
                     </div>
                     <div className="border-t-10 border-t-transparent border-b-10 border-b-transparent border-l-10 border-l-[var(--accent)]"></div>
                 </div>
-                <div className="py-25 lg:px-15 xl:px-25 w-[70%] p-8 overflow-hidden">
-                    <h1 className="text-[var(--p0)] text-4xl font-bold mb-4">{project.title}</h1>
-                    <p className="text-[var(--p2)]">{project.content}</p>
-                    <a href={project.projectLink}>Try it!</a>
-                    <img src={project.img} alt={project.imgAlt} />
+
+                <div className="my-10 w-[100%] sm:w-[85%] md:w-[75%] lg:w-[70%] p-1 sm:p-2 md:p-4 grid grid-cols-1 justify-items-center bg-[var(--accent)]">
+
+                    <div className="my-2 sm:my-5 py-10 md:py-25 p-6 sm:p-8 lg:px-10 xl:px-15 w-[99%] sm:w-[97%] md:w-[95%] overflow-hidden border-[var(--accent0)] border-2">
+                        <h1 className="text-[var(--p0)] text-4xl font-bold mb-4">{project.title}</h1>
+                        
+                        <ProgrammingLanguages languages = {project.languages}/>
+
+                        <div className="p-2">
+                            <p className="font-bold text-[var(--p2)] py-2">{project.description}</p>
+                            <p className="text-[var(--p2)]">{project.introduction}</p>
+                        </div>
+                        
+                        {/* Links for github and live project */}
+                        <div className="grid grid-cols-1 justify-items-center h-10">
+                            <div className="grid grid-cols-2 w-[50%]">
+                                <a href={project.demoLink}>
+                                    <motion.div className="bg-[var(--midground)] border-2 p-1 my-1 mx-3 text-center hover:bg-[var(--background)] hover:border-3 duration-100"
+                                        whileHover={{ scale: 1.1 }}
+                                        whileTap={{ scale: 0.9 }}
+                                        >
+                                        <p>See It In Action</p>
+
+                                    </motion.div>
+                                </a>
+                                <a href={project.gitLink}>
+                                    <motion.div className="border-2 p-1 my-1 mx-3 text-center hover:border-3 hover:border-[var(--background)] hover:text-[var(--background)] duration-100"
+                                        whileHover={{ scale: 1.1 }}
+                                        whileTap={{ scale: 0.9 }}
+                                        >
+                                        <p>Look Behind the Curtain</p>
+                                    </motion.div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <img className="sm:mx-2 mt-5 mb-2 h-auto w-auto border-2 border-[var(--accent1)]" src={project.img1} alt={project.imgAlt1} />
+                        <img className="sm:mx-2 mt-5 mb-2 h-auto w-auto border-2 border-[var(--accent1)]" src={project.img2} alt={project.imgAlt2} />
+                        <img className="sm:mx-2 mt-5 mb-2 h-auto w-auto border-2 border-[var(--accent1)]" src={project.img3} alt={project.imgAlt3} />
+                        <img className="sm:mx-2 mt-5 mb-2 h-auto w-auto border-2 border-[var(--accent1)]" src={project.img4} alt={project.imgAlt4} />
+                    </div>
                 </div>
             </div>
         </div>
+    );
+}
+
+// splits the information from the JSON file about what programming languages were used in development the the project
+function ProgrammingLanguages({languages}: {languages?:string}) {
+    if (!languages || languages.trim() === "") return null;
+    const words = languages.split(",");
+    const languagesList = words.map(word => 
+        <li key={word} className="pl-2">
+            {word}
+        </li>
+    )
+    return (
+        <>
+        <div className="flex border-2 w-[50%] p-2 bg-[var(--background)]">
+            <h2 className="text-2xl font-bold">Built Using:</h2>
+            <ul className="pl-10 flex text-[var(--p3)] items-center list-disc list-inside font-light text-sm">
+                {languagesList}
+            </ul>
+        </div>
+        </>
     );
 }
